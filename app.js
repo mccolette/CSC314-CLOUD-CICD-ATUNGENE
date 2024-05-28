9181121f7d4bdd4db5bd8330761b0628251e8cf8
MColette <etianghe@gmail.com>
2024-05-28 14:28:11 +0100

    NEW

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is CSC 314 cloud computing ATUNGENE MARY EGBEDE VUG/CSC/22/7599 DEVOPS ; CI-CD PIPELINE !'));
+app.get('/', (req, res) => res.send(' this is CSC 314 cloud computing ATUNGENE MARY EGBEDE VUG/CSC/22/7599 CLASS DEVOPS ; CI-CD PIPELINE !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
